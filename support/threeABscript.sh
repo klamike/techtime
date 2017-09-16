0842624b8bd2d97be309e4b39237eec945d563b7
Michael Klamkin <klamike@users.noreply.github.com>
2017-09-16 01:48:34 -0400

    Add files via upload

---
+++ support/threeABscript.sh
@@ -0,0 +1,6 @@
+sed -i.bu '/oneAsh/ s/1/0/' /Users/Mike/Desktop/Media/bitbar/techtime.1s.sh
+sed -i.bu '/twoAsh/ s/1/0/' /Users/Mike/Desktop/Media/bitbar/techtime.1s.sh
+sed -i.bu '/threeABsh/ s/0/1/' /Users/Mike/Desktop/Media/bitbar/techtime.1s.sh
+sed -i.bu '/fiveAsh/ s/1/0/' /Users/Mike/Desktop/Media/bitbar/techtime.1s.sh
+sed -i.bu '/sixAsh/ s/1/0/' /Users/Mike/Desktop/Media/bitbar/techtime.1s.sh
+find /Users/Mike/Desktop/Media/bitbar/ -type f -name '*.sh.bu' -delete
